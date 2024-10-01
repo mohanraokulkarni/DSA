8529752557b098364bbee766c47081fb7addd393
mohanraokulkarni <99826022+mohanraokulkarni@users.noreply.github.com>
2024-10-01 19:36:13 +0530

    Time: 222 ms (82.51%) | Memory: 19.6 MB (26.52%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -1,25 +1,30 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        sum_upto_dict={0:1}
-        prefix_sum=0
+        ls=nums
+        target=k
+        d={0:1}
         count=0
-        
-        for i in range(0,len(nums)):
-            prefix_sum=prefix_sum+nums[i]
 
-            diff= prefix_sum-k
 
-            if diff in sum_upto_dict:
-                #left=sum_upto_dict[diff]
-                count=count+ sum_upto_dict[diff]
+        prefix_sum=0
+        for i in range(0,len(ls)):
+            prefix_sum=prefix_sum+ls[i]
             
-            if prefix_sum not in sum_upto_dict:
-                sum_upto_dict[prefix_sum]=1
+            diff=prefix_sum-target
+            
+            if diff in d:
+                count=count+d[diff]
+            
+            if prefix_sum not in d:
+                d[prefix_sum]=1
             else:
-                sum_upto_dict[prefix_sum]=sum_upto_dict[prefix_sum]+1 
-
-        
+                d[prefix_sum]=d[prefix_sum]+1
         return count
+        
+        
+        
+    
+    
 
 
         
