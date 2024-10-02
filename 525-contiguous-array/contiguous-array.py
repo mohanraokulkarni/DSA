4d5c2195ff8e38034ee503937f725017e9d06992
mohanraokulkarni <99826022+mohanraokulkarni@users.noreply.github.com>
2024-10-02 22:16:06 +0530

    Time: 613 ms (67.67%) | Memory: 22.3 MB (10.86%) - LeetSync

--- 525-contiguous-array/contiguous-array.py
+++ 525-contiguous-array/contiguous-array.py
@@ -5,11 +5,10 @@ class Solution:
         prefix_sum=0
         max_length=0
 
-
         for i in range(0,len(nums)):
             if nums[i]==0:
                 prefix_sum=prefix_sum - 1
-            if nums[i]==1:
+            else:
                 prefix_sum=prefix_sum + 1
             
             if prefix_sum in d:
