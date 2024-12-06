1eecf59a9ba483d4bbc3a02866c01445c9c2d1c0
mohanraokulkarni <99826022+mohanraokulkarni@users.noreply.github.com>
2024-12-07 02:37:37 +0530

    Time: 627 ms (20.00%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -2,6 +2,7 @@
 SELECT w1.id
 FROM Weather w1 INNER JOIN Weather w2 # return the common part we can use any jin here
 ON DATEDIFF(w1.recordDate, w2.recordDate) = 1 AND w1.temperature > w2.temperature
-#WHERE DATEDIFF(w1.recordDate, w2.recordDate) = 1 AND w1.temperature > w2.temperature
 
+#WHERE DATEDIFF(w1.recordDate, w2.recordDate) = 1 AND w1.temperature > w2.temperature
+# WE CAN USE WHERE INSTED OF ON HERE 
 # DATEDIFF ----> GIVES THE DIFFERENCE BETWEEN TO DATES
