dc574db6615844f0e06370f94e74ada9d4fb18c6
mohanraokulkarni <99826022+mohanraokulkarni@users.noreply.github.com>
2024-12-07 01:43:24 +0530

    Time: 1548 ms (19.99%) | Memory: 0B (100.00%) - LeetSync

--- 1509-replace-employee-id-with-the-unique-identifier/replace-employee-id-with-the-unique-identifier.sql
+++ 1509-replace-employee-id-with-the-unique-identifier/replace-employee-id-with-the-unique-identifier.sql
@@ -1,3 +1,10 @@
 # Write your MySQL query statement below
 SELECT eu.unique_id AS unique_id , e.name AS name
 FROM Employees e LEFT JOIN EmployeeUNI eu ON e.id = eu.id
+
+# as ---> used to give a table name 
+# __Table_1___  LEFT JOIN    ___Table__2___   ON   ____CONDITION____
+# left join considers all the rows from the left table, 
+#and matchs the value based on condition and if some elements are not present and fill it with a null 
+# therefore we have all the names from the table1 and 
+#matches with the uniqe_id if table 2 has id same as the names mentioned in table 1 else null value is automatically filled 
