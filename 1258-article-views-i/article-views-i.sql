b193508ff8d8b2bd2f2afe84d78ccf7cf9c09463
mohanraokulkarni <99826022+mohanraokulkarni@users.noreply.github.com>
2024-12-07 01:09:27 +0530

    Time: 494 ms (34.86%) | Memory: 0B (100.00%) - LeetSync

--- 1258-article-views-i/article-views-i.sql
+++ 1258-article-views-i/article-views-i.sql
@@ -3,3 +3,7 @@ SELECT DISTINCT(author_id) as id
 FROM Views
 WHERE author_id = viewer_id 
 ORDER BY id ASC;
+
+#DISTINCT --> NO DUBLICATIONS
+# AS ---> RENAME THE COLUMN
+# ORDER BY __COL_NAME_____ ASC/DSC;
