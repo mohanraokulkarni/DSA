f22edcbf6ca5d939f540ecda10fa9f9aef0bedd7
mohanraokulkarni <99826022+mohanraokulkarni@users.noreply.github.com>
2024-12-07 02:20:19 +0530

    Time: 1321 ms (79.36%) | Memory: 0B (100.00%) - LeetSync

--- 1724-customer-who-visited-but-did-not-make-any-transactions/customer-who-visited-but-did-not-make-any-transactions.sql
+++ 1724-customer-who-visited-but-did-not-make-any-transactions/customer-who-visited-but-did-not-make-any-transactions.sql
@@ -3,3 +3,17 @@ SELECT v.customer_id, COUNT(v.customer_id) AS count_no_trans
 FROM Visits v LEFT JOIN Transactions t ON v.visit_id = t.visit_id
 WHERE t.transaction_id is NULL
 GROUP BY v.customer_id
+
+#step1 : creating a join table where we have for each visit there is transaction
+# is made or not ----> if no tansaction then the null value is filled automatically
+#step2:applying a condinton to select that visited but not transcation using where
+#step 3: selecting customer_id and new column with there count  and giving name using as 
+#| customer_id | count_no_trans |
+#| ----------- | -------------- |
+#| 30          | 4              |
+# group by is used to sub dividing for each group
+#| customer_id | count_no_trans |
+#| ----------- | -------------- |
+#| 30          | 1              |
+#| 96          | 1              |
+#| 54          | 2              |
